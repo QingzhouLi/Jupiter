63d3bd8c0faf7abec44f1b597fb383b2642b9e67
QingzhouLi <43625998+QingzhouLi@users.noreply.github.com>
2019-03-21 16:57:28 -0400

    Add files via upload

---
+++ Jupiter/src/rpc/SearchItem.java
@@ -0,0 +1,81 @@
+package rpc;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpSession;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import db.DBConnection;
+import db.DBConnectionFactory;
+import entity.Item;
+
+/**
+ * Servlet implementation class SearchItem
+ */
+@WebServlet("/search")
+public class SearchItem extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchItem() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		double lat = Double.parseDouble(request.getParameter("lat"));
+		double lon = Double.parseDouble(request.getParameter("lon"));
+
+		String term = request.getParameter("term");
+		DBConnection dbConnection = DBConnectionFactory.getConnection();
+		
+		try {
+//			HttpSession session = request.getSession(false);
+//			if(session == null) {
+//				response.setStatus(403);
+//				dbConnection.close();
+//				return;
+//			}
+//			
+//			String userId = session.getAttribute("user_id").toString();
+			String userId = request.getParameter("user_id");
+			List<Item> items = dbConnection.searchItems(lat, lon, term);
+			Set<String> favoritedItemIds = dbConnection.getFavoriteItemIds(userId);
+			JSONArray array = new JSONArray();
+			for (Item item : items) {
+				JSONObject obj = item.toJSONObject();
+				obj.put("favorite", favoritedItemIds.contains(item.getItemId()));
+				array.put(obj);
+			}
+			RpcHelper.writeJsonArray(response, array);
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}finally {
+	  		dbConnection.close();
+        }
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+}
